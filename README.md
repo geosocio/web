bf805f5158fe051414876d50c25f615bde7639c6
David Barratt <david@davidwbarratt.com>
2017-10-07 01:02:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# [![GeoSoc.io](https://avatars2.githubusercontent.com/u/27577422?v=3&s=25)](https://github.com/geosocio) Core
+
+[![Build Status](https://travis-ci.org/geosocio/core.svg?branch=develop)](https://travis-ci.org/geosocio/web) [![Coverage Status](https://coveralls.io/repos/github/geosocio/web/badge.svg?branch=develop)](https://coveralls.io/github/geosocio/web?branch=develop)
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+  filename: 'style.css',
+  disable: process.env.NODE_ENV !== 'production',
+});
+
+const config = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'html/bundles'),
+  },
+  devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'cheap-module-eval-source-map',
+  resolve: {
+    alias: {
+      app: path.resolve('./src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$|\.css$/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+          // use style-loader in development
+          fallback: 'style-loader',
+        }),
+      },
+    ],
+  },
+  plugins: [
+    extractSass,
+    new webpack.DefinePlugin({
+      APP_ENV: JSON.stringify(process.env.APP_ENV),
+      // @see https://facebook.github.io/react/docs/optimizing-performance.html#webpack
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+  ],
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(new UglifyJSPlugin({
+    sourceMap: true,
+  }));
+}
+
+module.exports = config;
