e950c8f8dced69751d2ba778b097d1fd9b34fe54
David Barratt <david@davidwbarratt.com>
2020-01-05 14:23:09 -0500

    Create placeholder

--- index.js
+++ index.js
@@ -1,38 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import createHistory from 'history/createBrowserHistory';
-import { ConnectedRouter, routerMiddleware, replace } from 'react-router-redux';
-// import App from './components/app';
-// import reducer from './reducers/index';
-import { createEpicMiddleware } from 'redux-observable';
-import { composeWithDevTools } from 'redux-devtools-extension';
-// import epic from './epics';
-// import './styles/styles.scss';
+import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
 
-function main() {
-  // Create a history of your choosing (we're using a browser history in this case)
-  const history = createHistory();
-  // Build the middleware for intercepting and dispatching navigation actions
-  const router = routerMiddleware(history);
-  const epicMiddleware = createEpicMiddleware(epic);
-  // Add the reducer to your store on the `router` key
-  // Also apply our middleware for navigating
-  const store = createStore(
-    reducer,
-    composeWithDevTools(applyMiddleware(router, epicMiddleware)),
-  );
+/**
+ * The DEBUG flag will do two things that help during development:
+ * 1. we will skip caching on the edge, which makes it easier to
+ *    debug.
+ * 2. we will return an error message on exception in your Response rather
+ *    than the default 404.html page.
+ */
+const DEBUG = false
 
-  ReactDOM.render(
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        {/* <App /> */}
-      </ConnectedRouter>
-    </Provider>,
-    document.getElementById('root')
-  );
+addEventListener('fetch', event => {
+  try {
+    event.respondWith(handleEvent(event))
+  } catch (e) {
+    if (DEBUG) {
+      return event.respondWith(
+        new Response(e.message || e.toString(), {
+          status: 500,
+        }),
+      )
+    }
+    event.respondWith(new Response('Internal Error', { status: 500 }))
+  }
+})
+
+async function handleEvent(event) {
+  const url = new URL(event.request.url)
+  let options = {}
+
+  /**
+   * You can add custom logic to how we fetch your assets
+   * by configuring the function `mapRequestToAsset`
+   */
+  // options.mapRequestToAsset = handlePrefix(/^\/docs/)
+
+  try {
+    if (DEBUG) {
+      // customize caching
+      options.cacheControl = {
+        bypassCache: true,
+      }
+    }
+    return await getAssetFromKV(event, options)
+  } catch (e) {
+    // if an error is thrown try to serve the asset at 404.html
+    if (!DEBUG) {
+      try {
+        let notFoundResponse = await getAssetFromKV(event, {
+          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/index.html`, req),
+        })
+
+        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 })
+      } catch (e) {}
+    }
+
+    return new Response(e.message || e.toString(), { status: 500 })
+  }
 }
 
-// Engage!
-main();
+/**
+ * Here's one example of how to modify a request to
+ * remove a specific prefix, in this case `/docs` from
+ * the url. This can be useful if you are deploying to a
+ * route on a zone, or if you only want your static content
+ * to exist at a specific path.
+ */
+function handlePrefix(prefix) {
+  return request => {
+    // compute the default (e.g. / -> index.html)
+    let defaultAssetKey = mapRequestToAsset(request)
+    let url = new URL(defaultAssetKey.url)
+
+    // strip the prefix from the path for lookup
+    url.pathname = url.pathname.replace(prefix, '/')
+
+    // inherit all other props from the default request
+    return new Request(url.toString(), defaultAssetKey)
+  }
+}
